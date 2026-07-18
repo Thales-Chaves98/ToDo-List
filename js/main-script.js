1f769cc29c08d072a5a708a8de091048621ce835
Thales <thales.augusto.chaves98@gmail.com>
2026-07-18 12:59:07 -0300

    Create form submit

--- js/main-script.js
+++ js/main-script.js
@@ -1,1 +1,10 @@
-const form = document.querySelector("task-form");
+const form = document.querySelector(".task-form");
+const inputTxt = document.getElementById("input-task")
+
+let taskTxt;
+
+form.addEventListener("submit",  (event) => {
+    event.preventDefault()
+    taskTxt = inputTxt.value;
+    console.log(taskTxt)
+});
