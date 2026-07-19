d292f674c9b0a422ffd531a44235a7d77edd8406
Thales <thales.augusto.chaves98@gmail.com>
2026-07-19 11:15:16 -0300

    Create Id for tasks  and checkbox logic

--- js/main-script.js
+++ js/main-script.js
@@ -1,36 +1,50 @@
 const form = document.querySelector(".task-form");
-const taskInput = document.getElementById("input-task")
+const taskInput = document.getElementById("input-task");
 
-const tasks = [];
+let tasks = [];
+let nextId = 1;
 
 form.addEventListener("submit",  (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const done = false;
     const taskName = taskInput.value.trim();
     
     if(taskName === ""){
-        console.log("vazio")
-        return
+        return;
     }
     const task = {
+        id: nextId,
         taskName,
         done,
     };
-    tasks.push(task)
+    tasks.push(task);
     taskInput.value = "";
+    nextId++;
     renderTasks();
+    
 });
 
 function renderTasks(){
-    const listContainer = document.getElementById("tasks-container")
-    listContainer.innerHTML = "";
+    const listContainer = document.getElementById("tasks-container");
+    listContainer.textContent = "";
 
     tasks.forEach((task) => {
         const taskLi = document.createElement("li");
+        taskLi.classList.add("task-item")
         taskLi.innerHTML = `
-        ${task.taskName}
+            <input type="checkbox" data-id="${task.id}" ${task.done ? "checked" : ""}>
+            <span class="task-name">${task.taskName}</span>
+            <button class="edit-btn">Editar</button>
+            <button class="delete-btn">Excluir</button>
         `;
-        listContainer.appendChild(taskLi)
-    })
+        listContainer.appendChild(taskLi);
+
+        const checkbox = taskLi.querySelector("input");
+        checkbox.addEventListener("change", () => {
+            task.done = !task.done;
+            renderTasks();
+            console.log(task.id + " " + task.done);
+        });
+    });
 }
